037ce79c98b6945605608b4b934b33373a74b3c4
Alec Krsek <aleckrsek@gmail.com>
2026-05-13 15:50:09 -0700

    Volcano signature

---
+++ homework5/volcano.erl
@@ -0,0 +1,5 @@
+-module(volcano).
+-export([main/0]).
+-export([new_volcano/4, name/1, elevation/1, last_eruption/1, hazard/1]).
+-export([load_volcanoes/1, sort_by_elevation/1, erupted_since/2, total_elevation/1]).
+
