91a0a33cdd1e4ee1649adc3ec23951ec5be70707
Alec Krsek <aleckrsek@gmail.com>
2026-05-20 13:15:52 -0700

    Implement new_volcano through load_volcanoes

---
+++ homework5/run.sh
@@ -0,0 +1,2 @@
+erlc volcano.erl 
+erl -noshell -s volcano main -s init stop
--- homework5/volcano.erl
+++ homework5/volcano.erl
@@ -1,6 +1,48 @@
 -module(volcano).
 -export([main/0]).
--export([new_volcano/4, name/1, elevation/1, last_eruption/1, hazard/1]).
--export([load_volcanoes/1, sort_by_elevation/1, erupted_since/2, total_elevation/1]).
+% -export([new_volcano/4, name/1, elevation/1, last_eruption/1, hazard/1]).
+% -export([load_volcanoes/1, sort_by_elevation/1, erupted_since/2, total_elevation/1]).
 
-new_volcano(Binary, Elevation, LastEruption, Hazard)
+new_volcano(Binary, Elevation, LastEruption, Hazard) ->
+    {volcano, Binary, Elevation, LastEruption, Hazard}.
+
+name({volcano, Name, _, _, _}) ->
+    Name.
+
+elevation({volcano, _, Elevation, _, _}) ->
+    Elevation.
+
+last_eruption({volcano, _, _, LastEruption, _}) ->
+    LastEruption.
+
+hazard({volcano, _, _, _, Hazard}) ->
+    Hazard.
+
+loader(Line) -> 
+    [Name, ElevationStr, LastEruptionStr, HazardStr] = binary:split(Line, <<",">>, [global]),
+    Elevation= binary_to_integer(ElevationStr),
+    LastEruption = binary_to_integer(LastEruptionStr),
+    Hazard = binary_to_integer(HazardStr),
+    new_volcano(Name, Elevation, LastEruption, Hazard).
+
+loader_loop([], Acc) -> Acc;
+loader_loop([Line | Rest], Acc) ->
+    case Line of
+        <<>> -> loader_loop(Rest, Acc); % skip empty lines, gives badmatch error
+        _ -> 
+            Volcano = loader(Line),
+            loader_loop(Rest, [Volcano | Acc])
+    end.
+
+
+load_volcanoes(File) ->
+    {ok, Data} = file:read_file(File),
+    Lines = binary:split(Data, <<"\n">>, [global]),
+    loader_loop(Lines, []).
+
+
+main()->
+    NewVolcano = new_volcano(<<"Mount Vesuvius">>, 1281, 79, high),
+    % io:format("New Volcano: ~p~n", [NewVolcano]),
+    LoadedVolcanoes = load_volcanoes("hw5_data.csv"),
+    io:format("Loaded Volcanoes: ~p~n", [LoadedVolcanoes]).
